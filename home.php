aa104e19e90c68f0537c55001815078b2aecfdcc
Enola Lamy <enola.vigneron54@gmail.com>
2025-06-23 18:19:48 +0200

    Ajout du hero avec photo aléatoire

--- home.php
+++ home.php
@@ -1,7 +1,27 @@
 <?php get_header(); ?>
 
-<main>
-    <h1>Bienvenue sur le site de Nathalie Mota</h1>
+<main id="home-hero">
+
+    <?php
+    // Requête d'une photo aléatoire
+    $random_photo = new WP_Query([
+        'post_type' => 'photo',
+        'posts_per_page' => 1,
+        'orderby' => 'rand',
+    ]);
+
+    if ($random_photo->have_posts()) :
+        while ($random_photo->have_posts()) : $random_photo->the_post();
+            $image_url = get_the_post_thumbnail_url(get_the_ID(), 'full');
+        endwhile;
+        wp_reset_postdata();
+    endif;
+    ?>
+
+    <section class="hero" style="background-image: url('<?php echo esc_url($image_url); ?>')">
+        <h1>PHOTOGRAPHE EVENT</h1>
+    </section>
+
 </main>
 
 <?php get_footer(); ?>
